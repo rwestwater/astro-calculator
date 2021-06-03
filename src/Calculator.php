f7e2e37e78b5056171f9d751e67be0725d7e77aa
Rachel Westwater <rachel-westwater@hotmail.com>
2021-06-03 21:35:48 +0100

    refactoring functionality

--- src/Calculator.php
+++ src/Calculator.php
@@ -31,41 +31,18 @@ class Calculator {
         {
             $planet_name = $planet->getPlanetName();
             // make an array of all planets and their masses using the uuid as the key
-            $max_array["$planet_name"] = $planet->getPlanetMass();
-            $farthest_away_planet = array_search(max($max_array), $max_array);
+            $max_array["$planet_name"] = $planet->getPlanetAstroUnit();
         }
-        
         // find the max integer of the planets
-        return $max_array;
+        $farthest_away_planet = max($max_array);
+        
+        return $farthest_away_planet;
     }
     
     public static function findTheDiameterOfTheSolarSystem($star) 
     {
         $farthest_away_planet = self::findTheFarthestAwayPlanet($star);
-        $solar_system_diameter = max($farthest_away_planet) * 2;
+        $solar_system_diameter = $farthest_away_planet * 2;
         return $solar_system_diameter;
     }
 }
-
-// $planet = new Planet("Rachel", 96, 100);
-// $planet_1 = new Planet("Granty", 900, 1000);
-// $planet_2 = new Planet("BAZZA", 9001, 20000);
-// $planet_3 = new Planet("BIG BOY", 50000, 20000);
-
-// $star = new Star("Joe", "3986596", 150);
-// $solar_system = new SolarSystem("J", "Jdawg69", 69);
-
-// $star->addPlanetToArray($planet);
-// $star->addPlanetToArray($planet_1);
-// $star->addPlanetToArray($planet_2);
-// $star->addPlanetToArray($planet_3);
-
-
-// $solar_system->addStarToSolarSystem($star);
-// print_r($solar_system);
-
-
-// $total_mass = Calculator::totalMassOfSolarSystem($solar_system);
-// print_r($total_mass);
-
-// Calculator::findTheFarthestAwayPlanet($star);
