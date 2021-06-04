6cba4f0da3844b8a905ae54efee224d65fcd8557
Rachel Westwater <rachel-westwater@hotmail.com>
2021-06-04 13:28:58 +0100

    all identity tests passing

--- tests/IdentityTest.php
+++ tests/IdentityTest.php
@@ -3,10 +3,11 @@
 namespace SolarSystem;
 
 use PHPUnit\Framework\TestCase;
+require 'models/Identity.php';
 
 class IdentityTest extends TestCase
 {
-    public function test_can_hydrate_from_existing_value()
+    public function testCanHydrateFromExistingValue()
     {
         $id = new Identity("44260076-3856-46ed-88d5-9946c093e6d7");
 
@@ -14,7 +15,7 @@ class IdentityTest extends TestCase
         $this->assertEquals("44260076-3856-46ed-88d5-9946c093e6d7", (string) $id);
     }
 
-    public function test_it_must_be_of_valid_uuid_4_format()
+    public function testItMustBeOfValidUuid4Format()
     {
         $this->expectException(\InvalidArgumentException::class);
         $this->expectExceptionMessage('Invalid UUID string: monkey');
