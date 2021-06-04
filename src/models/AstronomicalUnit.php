6d89c602492472749c3914c231ed557e1d3d6973
Rachel Westwater <rachel-westwater@hotmail.com>
2021-06-04 10:00:58 +0100

    adding get astro unit function

--- src/models/AstronomicalUnit.php
+++ src/models/AstronomicalUnit.php
@@ -4,8 +4,18 @@ namespace SolarSystem;
 
 final class AstronomicalUnit
 {
+
+    public $distance;
+
     public function __construct(float $distance)
     {
         $this->distance = $distance;
     }
+
+    // return the astronomical unit
+    public function getAstronomicalUnit() 
+    {
+        return $this->distance;
+    }
+
 }
